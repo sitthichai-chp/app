e951aaac379b91575df7d089e75ff3b3b6706769
dekpump <sakarin@webwarestudios.com>
2011-03-27 23:45:15 +0700

    เริิ่มต้นโปรเจ็ค - spree_paypal_express
    Signed-off-by: dekpump <sakarin@webwarestudios.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+App::Application.config.secret_token = '9f8dc7c57fc733afaad3f1954c68238a7324bf7e3a785399eeb41d909eb43208a8b95a38c955c99ff7343b42ad1cb866e6e82f4f40651ad9cfe89d6fa8d81c68'
